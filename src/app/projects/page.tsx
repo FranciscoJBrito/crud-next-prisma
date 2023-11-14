2ea4a489f6f3a7f72c9ba063a260237d43597e2d
Francisco Brito <franciscojavierbrito16@gmail.com>
2023-11-14 19:34:16 -0300

    adding notification sidebar and project cards

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,16 @@
+import NavBar from "@/components/NavBar/NavBar";
+import ProjectCard from "@/components/ProjectCard/ProjectCard";
+
 const Projects = () => {
   return (
-    <div>Hola desde proyectos</div>
-  )
-}
+    <div>
+      <NavBar />
+      <h2>Proyectos</h2>
+      <div className="w-full flex">
+        <ProjectCard />
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/ui/Button";
-import { BellIcon } from "@heroicons/react/20/solid";
 import MyModal from "../Modal/Modal";
+import NotificationSidebar from "../NotificationSidebar/NotificationSidebar";
 
 const NavBar = () => {
   return (
@@ -8,9 +8,7 @@ const NavBar = () => {
       <div className="text-base p-2 w-[250px] border-[1px] border-custom-gray rounded-lg text-custom-gray">search bar</div>
       <div className="flex items-center  w-fit">
         <Button text="Invitar" />
-        <div className="ml-3 p-2 bg-custom-gray rounded-lg cursor-pointer hover:bg-white/20">
-          <BellIcon className="h-6 w-6 text-white" />
-        </div>
+          <NotificationSidebar />
       </div>
     </div>
   );
